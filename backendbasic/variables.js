17fbb4d04888fe09dc5315fa4627b0339b954f2d
razzaksr <razzaksr@gmail.com>
2023-09-27 13:03:54 +0530

    js has begun

---
+++ backendbasic/variables.js
@@ -0,0 +1,38 @@
+// variable and types
+
+// const, var, let
+
+// keyword variable_name=value;
+
+// const department='cse'
+// var department='cse'
+let department='cse'
+
+console.log(typeof(department))
+
+department='IT'
+console.log(department)
+
+let myExp=12.5
+console.log(typeof(myExp))
+
+let myContact=8667002959787678
+console.log(typeof(myContact))
+
+// object/ java script object notation
+let myData={
+    'myName':'Razak Mohamed S',
+    'address':'Salem',
+    'contact':8667002959,
+    'projects':56
+}
+
+console.log(typeof(myData))
+
+let mySkills=['Spring Boot','Jinja','Thymeleaf','Node']
+
+console.log(typeof(mySkills))
+
+var myGroup=new Array(['Training','Development'])
+console.log(typeof(myGroup))
+
