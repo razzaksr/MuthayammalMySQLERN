03071d2ed3c9f74314d8f3b4deeb457b3e2fd2c8
razzaksr <razzaksr@gmail.com>
2023-10-03 12:59:14 +0530

    Express Get Mapping with MYSQL done

---
+++ ExpressCRUD/index.js
@@ -0,0 +1,87 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const mysql = require('mysql2')
+
+const dataBase = mysql.createConnection({
+    host:"localhost",
+    database:"muthayammal",
+    user:"root",
+    password:"Razak@123"
+})
+dataBase.connect(()=>{
+    console.log("Database connected successfully!!!!!!!!!!!!!")
+})
+const application=express()
+application.use(bodyParser.urlencoded({extended:true}))
+application.use(bodyParser.json())
+
+application.listen(2020,(err)=>{
+    console.log("Webservice running successfully")
+})
+
+
+// CRUD operations:
+
+// Read operation display all customers: get
+application.get('/list',async(req,res)=>{
+    const sql="select * from customers"
+    dataBase.query(sql,(err,records)=>{
+        if(err){
+            res.status(500).json({error:err.message})
+            return
+        }
+        if(records.length==0){
+            res.status(404).json({message:"No Customers are available"})
+            return
+        }
+        res.status(200).json(records)
+    })
+})
+
+// Read operation: read by primary key
+application.get('/find/:key',async(req,res)=>{
+    const sql="select * from customers where acc_number=?"
+    dataBase.query(sql,[req.params.key],(err,records)=>{
+        if(err){
+            res.status(500).json({error:err.message})
+            return
+        }
+        if(records.length==0){
+            res.status(404).json({message:"No Customers are available"})
+            return
+        }
+        res.status(200).json(records[0])
+    })
+})
+
+// Read operation: read by condition
+application.get('/balance/:amount',async(req,res)=>{
+    const sql="select * from customers where acc_bal>?"
+    dataBase.query(sql,[req.params.amount],(err,records)=>{
+        if(err){
+            res.status(500).json({error:err.message})
+            return
+        }
+        if(records.length==0){
+            res.status(404).json({message:"No Customers are available"})
+            return
+        }
+        res.status(200).json(records)
+    })
+})
+
+// Read operation: read by multiple condition
+application.get('/cash/:amount1/:amount2',async(req,res)=>{
+    const sql='select * from customers where acc_bal between ? and ?'
+    dataBase.query(sql,[req.params.amount1,req.params.amount2],(err,records)=>{
+        if(err){
+            res.status(500).json({error:err.message})
+            return
+        }
+        if(records.length==0){
+            res.status(404).json({message:"No Customers are available"})
+            return
+        }
+        res.status(200).json(records)
+    })
+})
--- expressbasic/dbconnect.js
+++ expressbasic/dbconnect.js
@@ -1,7 +1,11 @@
 const refExp=require("express")
 const refMysql=require("mysql2")
+const bodyParser=require('body-parser')
 
 const app=refExp()
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 const dbase=refMysql.createConnection({
     "host":"localhost",
     "user":"root",
@@ -31,4 +35,17 @@ app.get('/fetch',async(req,res)=>{
         }
         res.status(200).json({records})
     })
+})
+
+app.post('/new',async(req,res)=>{
+    const sql="insert into customers values(?,?,?)"
+    const{acc_number,acc_holder,acc_bal}=req.body
+    dbase.query(sql,[acc_number,acc_holder,acc_bal],(err,result)=>{
+        if(err){
+            res.status(404).json({"error":err.message})
+            return
+        }
+        res.status(200).json({message:"Account has opened",id:result.insertId})
+    })
+
 })
